3f9e4a587635f2f52408892efee18765a7c4d161
Rémy Godet <godetremy@free.fr>
2024-09-23 16:28:19 +0200

    feat(Chat): Create function for send / read / get recipient and support groups

--- src/services/pronote/chats.ts
+++ src/services/pronote/chats.ts
@@ -1,6 +1,6 @@
 import { PronoteAccount } from "@/stores/account/types";
 import { Chat, ChatMessage } from "../shared/Chat";
-import pronote from "pawnote";
+import pronote, {Discussion, DiscussionMessages, DiscussionRecipient, DiscussionSentMessage} from "pawnote";
 import { ErrorServiceUnauthenticated } from "../shared/errors";
 import { decodeAttachment } from "./attachment";
 import { Recipient } from "../shared/Recipient";
@@ -15,15 +15,52 @@ export const getChats = async (account: PronoteAccount): Promise<Array<Chat>> =>
 
   const studentName = account.instance.user.resources[0].name;
 
-  return chats.items.map((chat) => {
-    return {
+  async function getParticipantName (chat: Discussion): Promise<string> {
+    let recipients: Array<DiscussionRecipient> = await pronote.discussionRecipients(account.instance!, chat);
+    let recipientNames: Array<string> = recipients.map((recipient) => recipient.name);
+    recipientNames = recipientNames.filter((name) => !name.includes(studentName));
+    for (let i = 0; i < recipientNames.length; i++) {
+      recipientNames[i] = recipientNames[i]
+        .replaceAll("M. ", "")
+        .replaceAll("Mme ", "")
+        .split(" ")
+        .reverse()
+        .join(" ")
+      ;
+    }
+    if (recipientNames.length > 3) {
+      recipientNames = recipientNames.slice(0, 3);
+      recipientNames.push("et plus...");
+    }
+    return recipientNames.join(", ");
+  }
+
+  let result: Array<Chat> = [];
+  for (let chat of chats.items) {
+    let recipientName = chat.recipientName;
+    if (recipientName === undefined) {
+      recipientName = await getParticipantName(chat);
+    } else {
+      recipientName = recipientName
+        .replaceAll("M. ", "")
+        .replaceAll("Mme ", "")
+        .split(" ")
+        .reverse()
+        .join(" ")
+      ;
+    }
+    result.push({
       id: chat.participantsMessageID,
       subject: chat.subject,
       creator: chat.creator ?? studentName,
-      recipient: chat.recipientName ?? studentName,
-      _handle: chat
-    };
-  });
+      recipient: recipientName!,
+      unreadMessages: chat.numberOfMessagesUnread,
+      _handle: chat,
+      isGroup: chat.recipientName === undefined
+    });
+  }
+
+  return result;
 };
 
 export const getChatMessages = async (account: PronoteAccount, chat: Chat): Promise<Array<ChatMessage>> => {
@@ -59,9 +96,14 @@ export const createDiscussionRecipients = async (account: PronoteAccount): Promi
 
   const recipients = recipientsALL.flat();
   info("PRONOTE->createDiscussionRecipients(): OK", "pronote");
-
   return recipients.map((recipient) => ({
     name: recipient.name,
+    kind:
+        recipient.kind === pronote.EntityKind.Teacher ? "Professeur" :
+          recipient.kind === pronote.EntityKind.Personal ? "Personnel" :
+            recipient.kind === pronote.EntityKind.Student ? "Étudiant" : "Inconnu"
+    ,
+    description: recipient.subjects[0] ? recipient.subjects[0].name : "",
     _handle: recipient
   }));
 };
@@ -72,4 +114,20 @@ export const createDiscussion = async (account: PronoteAccount, subject: string,
 
   await pronote.newDiscussion(account.instance, subject, content, recipients.map(r => r._handle));
   info("PRONOTE->createDiscussion(): OK", "pronote");
+};
+
+export const sendDiscussionMessage = async (account: PronoteAccount, chat: Chat, content: string): Promise<void> => {
+  if (!account.instance)
+    throw new ErrorServiceUnauthenticated("pronote");
+
+  await pronote.discussionSendMessage(account.instance, chat._handle, content);
+  info("PRONOTE->discussionSendMessage(): OK", "pronote");
+};
+
+export const markDiscussionAsRead = async (account: PronoteAccount, chat: Chat): Promise<void> => {
+  if (!account.instance)
+    throw new ErrorServiceUnauthenticated("pronote");
+
+  await pronote.discussionRead(account.instance, chat._handle);
+  info("PRONOTE->markDiscussionAsRead(): OK", "pronote");
 };
