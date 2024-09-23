b2cd91fa09976a0b380f08c36b6c51870923187d
Rémy Godet <godetremy@free.fr>
2024-09-23 16:26:47 +0200

    feat(Chat): Update initalinidicator to support groups

--- src/components/News/InitialIndicator.tsx
+++ src/components/News/InitialIndicator.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { View } from "react-native";
 import { NativeText } from "../Global/NativeComponents";
+import {UsersRound} from "lucide-react-native";
 
 const InitialIndicator = ({ initial, color, textColor = "#FFF", size = 42 }: { initial: string, color: string, textColor: string, size?: number }) => {
   return (
@@ -13,12 +14,19 @@ const InitialIndicator = ({ initial, color, textColor = "#FFF", size = 42 }: { i
       alignItems: "center",
       backgroundColor: color,
     }}>
-      <NativeText variant="title" style={{
-        color: textColor,
-        fontSize: size / 2.25,
-      }}>
-        {initial.substr(0, 2).toUpperCase()}
-      </NativeText>
+      {initial === "group" ? (
+        <UsersRound size={size / 2} color={textColor}/>
+      ):(
+        <NativeText variant="title" style={{
+          color: textColor,
+          fontSize: size / 2.25,
+        }}>
+          {initial
+            .substr(0, 2)
+            .toUpperCase()
+          }
+        </NativeText>
+      )}
     </View>
   );
 };
