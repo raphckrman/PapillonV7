efa2f5c054317a10c09b84a29aca1c432a0b698b
raphckrman <41128238+raphckrman@users.noreply.github.com>
2024-11-11 00:48:44 +0100

    chore: remove unused dependencies in Lessons.tsx

--- src/views/account/Lessons/Lessons.tsx
+++ src/views/account/Lessons/Lessons.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import { FlatList, View, Dimensions, ViewToken } from "react-native";
-import { Button, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import type { Screen } from "@/router/helpers/types";
 import { useCurrentAccount } from "@/stores/account";
 import { useTimetableStore } from "@/stores/timetable";
