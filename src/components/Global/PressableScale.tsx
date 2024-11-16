e5de353bf279f483d2ed23db555c2e5d64319bb0
raphckrman <41128238+raphckrman@users.noreply.github.com>
2024-11-16 19:31:56 +0100

    fix(lint)

--- src/components/Global/PressableScale.tsx
+++ src/components/Global/PressableScale.tsx
@@ -93,7 +93,6 @@ export const PressableScale = forwardRef<View, PressableScaleProps>(
         delayPressIn={delayPressIn}
         onPressIn={onPressIn}
         onPressOut={onPressOut}
-        ref={ref}
       >
         <AnimatedView style={[style, animatedStyle]}>{children}</AnimatedView>
       </TouchableWithoutFeedback>
