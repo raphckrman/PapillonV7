3ad9587149a56a2b3def08f398d96d27809748d0
BORDIER-KERVRAN Gabriel <ggkervran@outlook.com>
2024-12-15 12:08:01 +0100

    feat(course/content): Add typing

--- src/services/shared/Timetable.ts
+++ src/services/shared/Timetable.ts
@@ -15,7 +15,17 @@ export interface TimetableClass {
   status?: TimetableClassStatus,
   statusText?: string,
   source?: string
-  url?: string
+  url?: string,
+  ressourceID?: string,
+  ressource?: {
+    title?: string,
+    description?: string,
+    category?: ResourceContentCategory,
+    files?: Array<{
+      name: string,
+      url: string
+    }>
+  }[]
 }
 
 export type Timetable = Array<TimetableClass>;
@@ -26,3 +36,27 @@ export enum TimetableClassStatus {
   ONLINE = "En ligne",
   TEST = "ds",
 }
+
+export enum ResourceContentCategory {
+  NONE = 0,
+  /** Corresponds to "Cours" */
+  LESSON = 1,
+  /** Corresponds to "Correction" */
+  CORRECTION = 2,
+  /** Corresponds to "Devoir sur table" */
+  DST = 3,
+  /** Corresponds to "Interrogation orale" */
+  ORAL_INTERROGATION = 4,
+  /** Corresponds to "Travaux dirigés" */
+  TD = 5,
+  /** Corresponds to "Travaux pratiques" */
+  TP = 6,
+  /** Corresponds to "Évaluation de compétences" */
+  EVALUATION_COMPETENCES = 7,
+  /** Corresponds to "EPI" */
+  EPI = 8,
+  /** Corresponds to "AP" */
+  AP = 9,
+  /** Corresponds to "Visio" */
+  VISIO = 12
+};
