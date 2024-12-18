729951a775a6e7d2d92231af6125395ecade0b63
JyhuKo <github@xbxbot.com>
2024-12-18 10:09:43 +0100

    Update Item.tsx

--- src/views/account/Homeworks/Atoms/Item.tsx
+++ src/views/account/Homeworks/Atoms/Item.tsx
@@ -27,6 +27,24 @@ interface HomeworkItemProps {
   navigation: NativeStackNavigationProp<RouteParameters, "HomeScreen" | "Homeworks", undefined>
 }
 
+export const timestampToString = (timestamp: number) => {
+  const date = new Date(timestamp);
+  const today = new Date();
+
+  today.setHours(0, 0, 0, 0);
+  date.setHours(0, 0, 0, 0);
+
+  const difference = Math.ceil((date.getTime() - today.getTime()) / (1000 * 60 * 60 * 24));
+
+  return difference === 0
+    ? "Aujourd'hui"
+    : difference === 1
+    ? "Demain"
+    : difference === 2
+    ? "Après-demain"
+    : `Dans ${difference} jours`;
+};
+
 const HomeworkItem = ({ homework, navigation, onDonePressHandler, index, total }: HomeworkItemProps) => {
   const theme = useTheme();
   const [subjectData, setSubjectData] = useState(getSubjectData(homework.subject));
@@ -69,23 +87,7 @@ const HomeworkItem = ({ homework, navigation, onDonePressHandler, index, total }
     setMainLoaded(true);
   }, [homework.done]);
 
-  const timestampToString = (timestamp: number) => {
-    const date = new Date(timestamp);
-    const today = new Date();
 
-    // heure par defaut a 00:00:00:00
-    today.setHours(0, 0, 0, 0);
-    date.setHours(0, 0, 0, 0);
-
-    const difference = Math.ceil((date.getTime() - today.getTime()) / (1000 * 60 * 60 * 24));
-    return difference === 0
-      ? "Aujourd'hui"
-      : difference === 1
-      ? "Demain"
-      : difference === 2
-      ? "Après-demain"
-      : `Dans ${difference} jours`;
-  };
 
   const renderCategoryOrReturnType = () => {
     if (category) {
