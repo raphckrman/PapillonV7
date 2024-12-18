e7b9ee54380a482b4ecb05009b08b159c19cdc11
JyhuKo <github@xbxbot.com>
2024-12-18 11:48:12 +0100

    Update Item.tsx

--- src/views/account/Homeworks/Atoms/Item.tsx
+++ src/views/account/Homeworks/Atoms/Item.tsx
@@ -28,23 +28,6 @@ interface HomeworkItemProps {
   navigation: NativeStackNavigationProp<RouteParameters, "HomeScreen" | "Homeworks", undefined>
 }
 
-export const timestampToString = (timestamp: number) => {
-  const date = new Date(timestamp);
-  const today = new Date();
-
-  today.setHours(0, 0, 0, 0);
-  date.setHours(0, 0, 0, 0);
-
-  const difference = Math.ceil((date.getTime() - today.getTime()) / (1000 * 60 * 60 * 24));
-
-  return difference === 0
-    ? "Aujourd'hui"
-    : difference === 1
-    ? "Demain"
-    : difference === 2
-    ? "Après-demain"
-    : `Dans ${difference} jours`;
-};
 
 const HomeworkItem = ({ homework, navigation, onDonePressHandler, index, total }: HomeworkItemProps) => {
   const theme = useTheme();
