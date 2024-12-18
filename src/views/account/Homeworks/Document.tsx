3d9f8226bf77655f4903118b5ef6cf7bd0bd84fa
JyhuKo <github@xbxbot.com>
2024-12-18 10:09:56 +0100

    Update Document.tsx

--- src/views/account/Homeworks/Document.tsx
+++ src/views/account/Homeworks/Document.tsx
@@ -28,6 +28,7 @@ import { AccountService } from "@/stores/account/types";
 import getAndOpenFile from "@/utils/files/getAndOpenFile";
 import { AutoFileIcon } from "@/components/Global/FileIcon";
 import { FileText, Link, Paperclip, CircleAlert, FileIcon } from "lucide-react-native";
+import { timestampToString } from "@/views/account/Homeworks/Atoms/Item";
 
 const HomeworksDocument: Screen<"HomeworksDocument"> = ({ route }) => {
   const theme = useTheme();
@@ -76,24 +77,6 @@ const HomeworksDocument: Screen<"HomeworksDocument"> = ({ route }) => {
     fetchSubjectData();
   }, [homework.subject]);
 
-
-  const timestampToString = (timestamp: number) => {
-    const date = new Date(timestamp);
-    const today = new Date();
-	  
-    today.setHours(0, 0, 0, 0);
-    date.setHours(0, 0, 0, 0);
-
-    const difference = Math.ceil((date.getTime() - today.getTime()) / (1000 * 60 * 60 * 24));
-
-    return difference === 0
-      ? "Aujourd'hui"
-      : difference === 1
-      ? "Demain"
-      : difference === 2
-      ? "Après-demain"
-      : `Dans ${difference} jours`;
-  };
 
   return (
     <View style={{ flex: 1 }}>
