e05e30dca0268b7f59d042bfc918370307f233d8
JyhuKo <github@xbxbot.com>
2024-12-18 01:21:16 +0100

    Update Item.tsx

--- src/views/account/Homeworks/Atoms/Item.tsx
+++ src/views/account/Homeworks/Atoms/Item.tsx
@@ -73,8 +73,18 @@ const HomeworkItem = ({ homework, navigation, onDonePressHandler, index, total }
     const date = new Date(timestamp);
     const today = new Date();
 
+    // heure par defaut a 00:00:00:00
+    today.setHours(0, 0, 0, 0);
+    date.setHours(0, 0, 0, 0);
+
     const difference = Math.ceil((date.getTime() - today.getTime()) / (1000 * 60 * 60 * 24));
-    return difference === 0 ? "Aujourd'hui" : difference === 1 ? "Demain" : difference === 2 ? "Après-demain" : `Dans ${difference} jours`;
+    return difference === 0
+      ? "Aujourd'hui"
+      : difference === 1
+      ? "Demain"
+      : difference === 2
+      ? "Après-demain"
+      : `Dans ${difference} jours`;
   };
 
   const renderCategoryOrReturnType = () => {
