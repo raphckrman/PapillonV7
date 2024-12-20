fb82332995bf08687294a04a15f11485d45ccb57
𝕂𝕪𝕝𝕚𝕒𝕟 <kylian.jarry@scaramouch.com>
2024-12-20 23:48:38 +0100

    fix: chargement des onglets incorrect lors du lancement

--- src/views/settings/SettingsTabs.tsx
+++ src/views/settings/SettingsTabs.tsx
@@ -120,22 +120,27 @@ const SettingsTabs = () => {
     const loadTabs = async () => {
       if (account.personalization.tabs) {
         const storedTabs = account.personalization.tabs;
-        const updatedTabs = defaultTabs
-          .filter((defaultTab) =>
-            storedTabs.some((storedTab) => storedTab.name === defaultTab.tab)
-          )
-          .map((defaultTab) => {
-            const storedTab = storedTabs.find((t) => t.name === defaultTab.tab);
-            return {
-              ...defaultTab,
-              enabled: storedTab ? storedTab.enabled : false,
-              installed: true,
-            };
-          });
 
-        const newTabsFound: Tab[] = defaultTabs.filter((defaultTab) => !storedTabs.some((storedTab) => storedTab.name === defaultTab.tab)).map((tab) => ({ ...tab, installed: true }));
+        const updatedTabs: Tab[] = storedTabs.map((storedTab) => {
+          const defaultTab = defaultTabs.find((defaultTab) => defaultTab.tab === storedTab.name);
+          return {
+            tab: storedTab.name,
+            label: defaultTab?.label || "Default Label",
+            icon: defaultTab?.icon || null,
+            enabled: storedTab.enabled,
+            installed: true,
+          };
+        });
 
-        setTabs(updatedTabs);
+        const newTabsFound: Tab[] = defaultTabs
+          .filter((defaultTab) => !storedTabs.some((storedTab) => storedTab.name === defaultTab.tab))
+          .map((tab) => ({
+            ...tab,
+            installed: true,
+            enabled: false,
+          }));
+
+        setTabs([...updatedTabs, ...newTabsFound]);
         setNewTabs(newTabsFound);
         setShowNewTabsNotification(newTabsFound.length > 0);
       } else {
