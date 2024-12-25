8c36cae9012216180ede84a36bffd87618161f0b
JyhuKo <github@xbxbot.com>
2024-12-25 23:12:36 +0100

    conbflct

--- src/views/account/Lessons/Document.tsx
+++ src/views/account/Lessons/Document.tsx
@@ -174,7 +174,7 @@ const LessonDocument: Screen<"LessonDocument"> = ({ route, navigation }) => {
         {
           icon: <PersonStanding />,
           text: lesson.teacher?.includes(",") ? "Professeurs" : "Professeur",
-          value: lesson.teacher,
+          value: lesson.teacher || "Aucun",
           enabled: Boolean(lesson.teacher?.trim()), // Check if lesson.teacher exists, is not null, and not empty
         },
         {
