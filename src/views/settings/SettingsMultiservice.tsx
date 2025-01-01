c5ee85d94091b1a2e084486f70d61114ed264e93
camarm-dev <contact@camarm.dev>
2025-01-01 19:04:09 +0100

    feat: created new settings screen

---
+++ src/views/settings/SettingsMultiservice.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Text, ScrollView, View, StyleSheet, Switch } from "react-native";
+import { useTheme } from "@react-navigation/native";
+import type { Screen } from "@/router/helpers/types";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import important_json from "@/utils/magic/regex/important.json"; // Ensure this file contains valid regex patterns
+import MagicContainerCard from "@/components/Settings/MagicContainerCard";
+import { NativeIcon, NativeIconGradient, NativeItem, NativeList, NativeListHeader, NativeText } from "@/components/Global/NativeComponents";
+import { PlugZap } from "lucide-react-native";
+import { useCurrentAccount } from "@/stores/account";
+
+const SettingsMultiservice: Screen<"SettingsMultiservice"> = ({ navigation }) => {
+  const theme = useTheme();
+  const { colors } = theme;
+  const insets = useSafeAreaInsets();
+  const account = useCurrentAccount(store => store.account);
+  const mutateProperty = useCurrentAccount(store => store.mutateProperty);
+
+  return (
+    <ScrollView
+      contentContainerStyle={{
+        paddingHorizontal: 15,
+      }}
+    >
+      {/*<MagicContainerCard theme={theme} />*/}
+
+      <NativeListHeader label="Options" />
+      <NativeList>
+        <NativeItem
+          trailing={
+            <Switch
+              value={account?.personalization?.MagicNews ?? false}
+              onValueChange={(value) => mutateProperty("personalization", { MagicNews: value })}
+            />
+          }
+          leading={
+            <NativeIconGradient
+              icon={<PlugZap />}
+              colors={["#04ACDC", "#6FE3CD"]}
+            />
+          }
+        >
+          <NativeText variant="title">
+            Multiservice
+          </NativeText>
+          <NativeText variant="subtitle">
+            Connecte plusieurs services différents en un seul environnement virtuel, en associant chaque fonctionnalité à un service connecté.
+          </NativeText>
+        </NativeItem>
+      </NativeList>
+    </ScrollView>
+  );
+};
+
+
+
+export default SettingsMultiservice;
