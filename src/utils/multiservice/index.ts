d743e730e13f3b8ed9d9ce2d7bbe714ebf849a55
camarm-dev <contact@camarm.dev>
2025-01-02 19:01:23 +0100

    fix: removed breaking hooks code

--- src/utils/multiservice/index.ts
+++ src/utils/multiservice/index.ts
@@ -4,9 +4,9 @@ import {useMultiService} from "@/stores/multiService";
 import {PrimaryAccount} from "@/stores/account/types";
 
 export function getFeatureAccount (feature: MultiServiceFeature, spaceLocalID: string) {
-  const accounts = useAccounts().accounts;
-  const accountId = useMultiService().getFeatureAccountId(feature, spaceLocalID);
-  const space = useMultiService().spaces.find(space => space.accountLocalID === spaceLocalID);
+  const accounts = useAccounts.getState().accounts;
+  const accountId = useMultiService.getState().getFeatureAccountId(feature, spaceLocalID);
+  const space = useMultiService.getState().spaces.find(space => space.accountLocalID === spaceLocalID);
   const featureAccount = accounts.find(account => account.localID === accountId) as PrimaryAccount;
 
   if (!space || !featureAccount || !accountId) return undefined;
