ad5509c1dab133823825b68457de20655061f8d8
Gabriel29306 <ggkervran@outlook.com>
2025-01-11 23:15:32 +0100

    chore(dev): remove dev logs
    Signed-off-by: Gabriel29306 <ggkervran@outlook.com>

--- src/views/settings/SettingsAbout.tsx
+++ src/views/settings/SettingsAbout.tsx
@@ -26,7 +26,6 @@ const SettingsAbout: Screen<"SettingsAbout"> = ({ navigation }) => {
   const fetchContributors = async () => {
     const fetchedContributors = await getContributors();
     setContributors(fetchedContributors);
-    console.log(fetchedContributors[0]);
   };
 
   useEffect(() => {
