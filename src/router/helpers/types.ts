e65b8a33375c72a351f14be85fbe7a54ab57e33e
Kgeek33 <kylian.jarry@scaramouch.com>
2025-02-04 15:41:56 +0100

    feat: mise à jour du typage de `PapillonNavigation`

--- src/router/helpers/types.ts
+++ src/router/helpers/types.ts
@@ -14,6 +14,7 @@ import {Client} from "pawrd";
 import { Host } from "turboself-api";
 import {Evaluation} from "@/services/shared/Evaluation";
 import { ThemesMeta } from "@/utils/chat/themes/Themes.types";
+import { TimetableClass } from "@/services/shared/Timetable";
 
 export type RouteParameters = {
   // welcome.index
@@ -86,7 +87,7 @@ export type RouteParameters = {
     title?: string;
     autoAdd?: boolean;
   };
-  LessonDocument: { lesson: Homework };
+  LessonDocument: { lesson: TimetableClass };
 
   Homeworks?: { outsideNav?: boolean };
   HomeworksDocument: { homework: Homework };
