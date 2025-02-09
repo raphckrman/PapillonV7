6664d5ca887e6d337e043530dc1b7ca6b5b5de60
Kgeek33 <kylian.jarry@scaramouch.com>
2025-02-09 16:08:31 +0100

    feat(onlineStatus): reconnexion au compte quand l'utilisateur se reconnecte à Internet

--- src/hooks/useOnlineStatus.tsx
+++ src/hooks/useOnlineStatus.tsx
@@ -13,8 +13,13 @@ import Reanimated, {
 } from "react-native-reanimated";
 import { WifiOff } from "lucide-react-native";
 import { animPapillon } from "@/utils/ui/animations";
+import { useCurrentAccount } from "@/stores/account";
 
 const useOnlineStatus = () => {
+  const account = useCurrentAccount((store) => store.account!);
+  const switchTo = useCurrentAccount((store) => store.switchTo);
+
+  const [isAlreadyOffline, setIsAlreadyOffline] = useState(false);
   const [isOnline, setIsOnline] = useState(true);
   const errorTitle = useMemo(() => getErrorTitle(), []);
 
@@ -25,6 +30,17 @@ const useOnlineStatus = () => {
 
     return () => unsubscribe();
   }, []);
+
+  useEffect(() => {
+    const reloadAccount = async () => await switchTo(account);
+
+    if (isOnline && isAlreadyOffline) {
+      reloadAccount();
+      setIsAlreadyOffline(false);
+    } else if (!isOnline) {
+      setIsAlreadyOffline(true);
+    }
+  }, [isOnline]);
 
   return { isOnline, errorTitle };
 };
