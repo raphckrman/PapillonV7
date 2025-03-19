1d8ab41caed62a6b27e1a7b376c2f12b85d212ee
Kgeek33 <kylian.jarry@scaramouch.com>
2025-03-19 16:55:00 +0100

    fix: add `isToday` and `isTomorrow` in DateHelper

--- src/utils/format/DateHelper.ts
+++ src/utils/format/DateHelper.ts
@@ -1,10 +1,19 @@
-import { formatDistance } from "date-fns";
+import { formatDistance, isToday, isTomorrow } from "date-fns";
 import { fr } from "date-fns/locale";
 
 export const timestampToString = (timestamp: number) => {
   if (isNaN(timestamp)) {
     return;
   }
+
+  if (isToday(timestamp)) {
+    return "Aujourd’hui";
+  }
+
+  if (isTomorrow(timestamp)) {
+    return "Demain";
+  }
+
   return formatDistance(new Date(timestamp), new Date(), {
     locale: fr,
     addSuffix: true,
