4a22a8489c4f27caf7e3bfb55f811e3ffc52b1e1
Vince Linise <vincelinise@gmail.com>
2025-03-20 12:58:15 +0100

    refactor: simplification de l'appel à la fonction operations dans history.ts

--- src/services/izly/history.ts
+++ src/services/izly/history.ts
@@ -3,11 +3,7 @@ import type { ReservationHistory } from "../shared/ReservationHistory";
 import { operations, OperationKind} from "ezly";
 
 export const history = async (account: IzlyAccount): Promise<ReservationHistory[]> => {
-  const payments = await operations(
-    account.instance!,
-    OperationKind.Payment,
-    200
-  );
+  const payments = await operations(account.instance!, OperationKind.Payment, 200);
   const topup = await operations(account.instance!, OperationKind.TopUp, 200);
   const currency = account.authentication.configuration.currency;
 
